c90081d71ec4b0036fa1204b0b1e1c4a5598180a
Ryan Febriansyah <15523163@students.uii.ac.id>
2022-04-03 15:57:43 +0700

    fixed mocking test for custom auth modules

--- tests/test_custom_auth.py
+++ tests/test_custom_auth.py
@@ -1,14 +1,17 @@
 import unittest
 import requests
-import requests_mock
+import requests_mock  # type: ignore
 from maritest.custom_auth import ApiKeyAuth, BasicAuth, BearerAuth, BasicAuthToken
 
 
 class TestCustomAuth(unittest.TestCase):
     def test_bearer_auth(self):
         with requests_mock.mock() as m:
-            m.get("https://github.com", request_headers={"Authorization" : "Bearer testtest"})
-        
+            m.get(
+                "https://github.com",
+                request_headers={"Authorization": "Bearer testtest"},
+            )
+
         bearer_auth = BearerAuth("testtest")
         response = requests.get("https://github.com", auth=bearer_auth)
 
@@ -16,24 +19,38 @@ class TestCustomAuth(unittest.TestCase):
 
     def test_basic_auth_token(self):
         with requests_mock.mock() as m:
-            m.get("https://github.com", request_headers={"Authorization" : "Basic password token"})
+            m.get(
+                "https://github.com",
+                request_headers={"Authorization": "Basic password token"},
+            )
 
         basic_auth = BasicAuthToken(token="password token")
         response = requests.get("https://github.com", auth=basic_auth)
 
         self.assertTrue(response.status_code, 200)
 
-    @unittest.skip("temporary skip mock test for API key auth")
     def test_api_key_auth(self):
-        # TODO: find a way to mocking test
-        # for API key auth
         with requests_mock.mock() as m:
             m.get(requests_mock.ANY, text="Success Mock API Key Auth")
 
-        api_key_auth = ApiKeyAuth("key", "value", "headers")
+        api_key_auth = ApiKeyAuth(
+            add_to="headers", key="key-1", value="value-1", header_name="Authorization"
+        )
         response = requests.get("https://github.com", auth=api_key_auth)
 
         self.assertTrue(response.status_code, 200)
+
+    def test_query_api_auth(self):
+        with requests_mock.mock() as m:
+            m.get(requests_mock.ANY, text="Success Mock API with Query Params")
+
+        query_api_auth = ApiKeyAuth(add_to="query_params", key="key1", value="value2")
+        response = requests.get("https://github.com", auth=query_api_auth)
+
+        self.assertTrue(response.status_code, 200)
+        self.assertEqual(
+            response.url, "https://github.com/?key1=value2", "Full-path url is equal"
+        )
 
     def test_basic_auth(self):
         with requests_mock.mock() as m:
@@ -47,4 +64,3 @@ class TestCustomAuth(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
