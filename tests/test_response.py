9ef9df297d0a532a37bf503f0d05eaa6c6ba8228
Ryan Febriansyah <15523163@alumni.uii.ac.id>
2022-04-11 14:13:19 +0700

    add negative scenario for module response

--- tests/test_response.py
+++ tests/test_response.py
@@ -29,6 +29,27 @@ class TestHttpResponse(unittest.TestCase):
         )
         response.history_response()
 
+    @unittest.expectedFailure
+    def test_invalid_format_argument(self):
+        response = Response(
+            method="GET",
+            url="https://jsonplaceholder.typicode.com/posts",
+            headers={"some_key": "some_value"},
+            proxies=None,
+            logger=False,
+        )
+        response.retriever(format="headers")
+
+    @unittest.expectedFailure
+    def test_invalid_type_argument(self):
+        response = Response(
+            method="GET",
+            url="https://jsonplaceholder.typicode.com/posts",
+            headers={"some_key": "some_value"},
+            proxies=None,
+            logger=False,
+        )
+        response.retriever(format=1234)
 
 if __name__ == "__main__":
     unittest.main()
