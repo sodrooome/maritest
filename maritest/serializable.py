b748b0328dbf1ef1148f6e54851ffbb0f5787f2f
Ryan Febriansyah <15523163@alumni.uii.ac.id>
2022-04-12 10:49:18 +0700

    create custom class for JSON serializable

---
+++ maritest/serializable.py
@@ -0,0 +1,19 @@
+class JsonSerializer(dict):
+    """
+    Simple JSON-serializable class that only
+    inherited from dict type built-ins. Most likely
+    will works for all basic data representation (not)
+    a complex one. For example ::
+
+    x = JsonSerializer(obj={"key":"value"})
+
+    :param json_object: Any object that which want
+    to be formatted into JSON object
+
+    Returned as a pair of key-value like JSON-type,
+    empty argument will resulted as empty JSON
+    """
+
+    def __init__(self, object):
+        if object is not None:
+            dict.__init__(self, object=object)
